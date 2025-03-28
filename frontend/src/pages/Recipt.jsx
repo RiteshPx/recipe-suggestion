17d94c9f21508701a470a42cf68b785e7981cf0d
Jeevan parmar <jeevnaparmar545@gmail>
2025-03-28 15:40:37 +0530

    add CARD

--- frontend/src/pages/Recipt.jsx
+++ frontend/src/pages/Recipt.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom';
+import RecipetCard from './RecipetCard';
+import Navbar from '../shared/Navbar'
+import { useNavigate } from 'react-router-dom';
+import { CTAButton } from '../components/cors/CTAButton';
 
 function Recipt() {
-    
-const location = useLocation();
-const data  = location.state || {};
-console.log(data)
+  const navigate = useNavigate();
+  const location = useLocation();
+  const data = location.state || {};
+  console.log(data);
+  const [Data, setData] = React.useState(data);
 
 
   return (
-    <div>Recipt
-       
+    <div className='bg-orange-400'>
+      <div className="flex flex-wrap ml-11 mr-11 gap-6 justify-center p-6 w-[90%] mx-auto">
+        {Data.map((recipe, index) => (
+          <RecipetCard key={index} recipe={recipe} />
+        ))}
+
+      </div>
+      <div className='flex justify-center items-center pb-4 '>
+        <CTAButton active={true} linkto={"/suggest"}> 
+          BACK
+        </CTAButton>
+
+      </div>
+
     </div>
   )
 }
